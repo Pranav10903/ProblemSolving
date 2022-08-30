35292111aac6360d7232c83574f544cbfdbe0cab
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2022-08-30 19:43:07 +0530

    Update unionOf2SortedArrays.cpp

--- Arrays/Easy/unionOf2SortedArrays.cpp
+++ Arrays/Easy/unionOf2SortedArrays.cpp
@@ -1,20 +1,47 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
 
-int search(int arr[],int n,int num)
-{
-    int i;
-    for(i=0;i<n;i++)
+using namespace std;
+
+vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
+  int i = 0, j = 0; // pointers
+  vector < int > Union; // Uninon vector
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
     {
-        if(arr[i]==num)
-        return i;
+      if (Union.size() == 0 || Union.back() != arr1[i])
+        Union.push_back(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.back() != arr2[j])
+        Union.push_back(arr2[j]);
+      j++;
     }
-    return -1;
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.back() != arr1[i])
+      Union.push_back(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.back() != arr2[j])
+      Union.push_back(arr2[j]);
+    j++;
+  }
+  return Union;
 }
+
 int main()
+
 {
-    int arr[]={1,2,3,4,5};
-    int num = 4;
-    int n = sizeof(arr)/sizeof(arr[0]);
-    int val = search(arr,n,num);
-    printf("%d",val);
+  int n = 10, m = 7;
+  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int arr2[] = {2, 3, 4, 4, 5, 11, 12}; 
+  vector < int > Union = FindUnion(arr1, arr2, n, m);
+  cout << "Union of arr1 and arr2 is  " << endl;
+  for (auto & val: Union)
+    cout << val << " ";
+  return 0;
 }
